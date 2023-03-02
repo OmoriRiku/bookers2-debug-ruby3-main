4eff0d937e1b984f5a569a91e75e9714c5a22c7c
OmoriRiku <oomori3321@gmail.com>
2023-03-02 10:33:22 +0000

    [Add]レビュー機能の追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_02_26_111612) do
+ActiveRecord::Schema.define(version: 2023_03_02_080536) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 2023_02_26_111612) do
     t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "star"
   end
 
   create_table "favorites", force: :cascade do |t|
